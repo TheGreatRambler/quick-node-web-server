9167dff986eab32635b28312af2bef55b95376f1
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-05-15 10:40:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var serveStatic = require('serve-static');
 var path = require('path');
+var compression = require('compression')
 var app = express();
 var directory = path.join(__dirname, "public");
+
+app.use(compression({
+	level: 5
+}));
 
 app.use(serveStatic(directory, {
 	'index': ["index.html"]
 }));
 
 app.use(function(req, res, next) {
-  console.log("A 404!");
+	console.log("A 404!");
 });
 
 app.listen(80);
